b1f53c8bcdb20491e4c2f3e4516f187ffa42ddb4
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2018-04-22 11:44:11 +0100

    Add files via upload

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,13 +1,18 @@
 // count degree and rotate clock hands;
 const rotateHands = function(str, num){
-    const hands = document.getElementsByClassName('hand-clock'),
+    let hands = document.getElementsByClassName('hand-clock'),
           dateContainer = document.querySelector('.date-box'),
           // count current degree;
           seconds = 6 * Number(str.split(':').splice(2, 1)) - 90,
           minutes = 6 * Number(str.split(':').splice(1, 1)) - 90,
-          hours = 60 * (Number((str.split(':').splice(0, 1))) + minutes / 60) - 90,
+          hours = 30 * ((str.split(':').splice(0, 1) % 12) + str.split(':').splice(1, 1) / 60) - 90,
           date = num.split('/').splice(0, 1);
           console.log(hours);
+    
+//    seconds = 180;
+//    minutes = 90;
+//    hours = ;
+    
     hands[0].style.transform = 'rotate(' + seconds + 'deg)';
     hands[1].style.transform = 'rotate(' + minutes + 'deg)';
     hands[2].style.transform = 'rotate(' + hours + 'deg)';
